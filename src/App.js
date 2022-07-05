3378c7582fdccc090915de94eb7c1578154f2137
vikaskumarsardar <swapankumarsardar73727@gmail.com>
2022-07-06 00:36:00 +0530

    first edit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+const socket = io("http://localhost:9000");
 function App() {
+  const [messageArray, setMessageArray] = useState([]);
+  const [data,setData] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setMessageArray(res=>[data,...res])
+    socket.emit("chat message", data);
+  };
+  
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setData(value);
+  };
+
+  socket.on("chat message", (incomingData) =>{
+    setMessageArray([incomingData,...messageArray])
+  } )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>hero hoon main</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} />
+          </form>
+        <div>
+          <h2>{messageArray.length}</h2>
+        {messageArray.map((res,id)=>{
+          return (<h2 key={id}>{res}</h2>)
+        })}
+        </div>
     </div>
   );
 }
