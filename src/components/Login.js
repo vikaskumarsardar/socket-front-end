0eecbcb0d56812372463cdebc6e58c840890417c
swapankumarsardar <swapankumarsardar73727@gmail.com>
2022-07-06 18:25:46 +0530

    added few things

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,16 @@
 import React, { useState, } from "react";
-import {useNavigate} from 'react-router-dom'
+import {Navigate, useNavigate} from 'react-router-dom'
 import axios from "axios";
+import Socket from "./Socket";
 export default function Login() {
   const [data, setData] = useState({
     username: "",
     password: "",
     countryCode: "",
   });
+
   const navigate = useNavigate()
+  const [logged,setLogged] = useState(false)
   const handleChange = (e) => {
     const { value, name } = e.target;
     setData((res) => {
@@ -20,11 +23,11 @@ export default function Login() {
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
-
       const result = await axios.post(
-        "http://localhost:9000/api/messages/login",
+        "http://localhost:5000/api/messages/login",
         data
       );
+      localStorage.setItem('token',JSON.stringify(result.data.data.accessToken))
       setData({
         username: "",
         password: "",
@@ -33,12 +36,28 @@ export default function Login() {
 
 
       if(result.data){
-        navigate("/socket")
+        // setLogged(true)
+        // navigate('/socket')
       }
     } catch (error) {
-      console.log(error);
+      console.log("login Error", error);
     }
   };
+  const handleClick = async() =>{
+    try{
+
+      const result = await axios.post("http://localhost:5000/api/messages/test",{},{
+        headers : {
+          "authorization" : `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+        }
+      })
+      console.log("result" ,result);
+    }catch(error){
+      console.log("test Error",error);
+    }
+  }
+  
+  
 
   return (
     <>
@@ -66,6 +85,7 @@ export default function Login() {
         />
         <button type="submit">Submit Now</button>
       </form>
+      <button onClick={handleClick}>click now</button>
     </>
   );
 }
--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,40 +1,54 @@
-import React ,{useState} from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
-const socket = io("http://localhost:9000");
+const socket = io("http://localhost:5000");
 
 export default function Socket() {
-    
   const [messageArray, setMessageArray] = useState([]);
-  const [data,setData] = useState('')
+  const [data, setData] = useState("");
+  const [logged, setLogged] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // async function isVerified(){
+    //   const isUserAuthenticated = await axios.post('http://localhost:5000/api/messages/')
+
+    // }
+    
+    
+    setLogged(true);
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setMessageArray(res=>[data,...res])
+    setMessageArray((res) => [data, ...res]);
     socket.emit("chat message", data);
   };
-  
+
   const handleChange = (e) => {
     const { value } = e.target;
     setData(value);
   };
 
-  socket.on("chat message", (incomingData) =>{
-    setMessageArray([incomingData,...messageArray])
-  } )
+  socket.on("chat message", (incomingData) => {
+    setMessageArray([incomingData, ...messageArray]);
+  });
 
   return (
-    <div className="App">
-      <h2>hero hoon main</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" onChange={handleChange} />
-          </form>
+    logged && (
+      <div className="App">
+        <h4>Messsaging App</h4>
+        <form onSubmit={handleSubmit} spellCheck={false}>
+          <input type="text" onChange={handleChange} />
+        </form>
         <div>
           <h2>{messageArray.length}</h2>
-        {messageArray.map((res,id)=>{
-          return (<h2 key={id}>{res}</h2>)
-        })}
+          {messageArray.map((res, id) => {
+            return <h2 key={id}>{res}</h2>;
+          })}
         </div>
-    </div>
+      </div>
+    )
   );
-
 }
